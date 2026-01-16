f7616861656ebec5444b31a939e6b7047ddbd3e9
NataFB <natafbt@gmail.com>
2026-01-16 13:17:27 -0300

    Subindo exercicios da lista de exercicios

---
+++ ExerciciosEstruturaSequenciais/ExercicioProposto5/ExercicioProposto5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioProposto5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o código da Primeira peça: ");
+            int peca1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade da Primeira peça: ");
+            int qtd1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor unitário da Primeira peça: ");
+            double valor1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o código da Segunda peça: ");
+            int peca2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade da Segunda peça: ");
+            int qtd2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor unitário da Segunda peça: ");
+            double valor2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = (qtd1 * valor1) + (qtd2 * valor2);
+            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ ExerciciosEstruturaSequenciais/ExercicioProposto6/ExercicioProposto6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioProposto6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double A, B, C, triangulo, circulo, trapezio, quadrado, retangulo;
+
+            Console.WriteLine("Digite os valores de cada lado do triangulo (na mesma linha): ");
+            string[] valores = Console.ReadLine().Split(' ');
+
+            A = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            B = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            triangulo = (A * C) / 2.0;
+            circulo = 3.14159 * Math.Pow(C, 2);
+            trapezio = ((A + B) * C) / 2.0;
+            quadrado = B * B;
+            retangulo = A * B;
+
+            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+
+}
