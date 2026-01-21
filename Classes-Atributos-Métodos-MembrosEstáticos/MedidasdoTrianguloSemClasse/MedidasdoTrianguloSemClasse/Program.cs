7e9156a8a9035cfe6db80f02586891aad6c14fdb
NataFB <natafbt@gmail.com>
2026-01-21 11:04:03 -0300

    Reorganizando pasta do curso

---
+++ Classes-Atributos-Métodos-MembrosEstáticos/MedidasdoTrianguloSemClasse/MedidasdoTrianguloSemClasse/Program.cs
@@ -0,0 +1,43 @@
+﻿// Fazer um programa para ler os lados de dois triângulos X e Y (suponha medidas válidas). Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos possui a maior área.
+// formula para calcular a area do triangulo = area = raiz quadrada de p * (p - a) * (p - b) * (p - c) onde p = (a + b + c) / 2
+
+using System;
+using System.Globalization;
+
+namespace MedidasdoTrianguloSemClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double pX = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(pX * (pX - xA) * (pX - xB) * (pX - xC));
+
+            double pY = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(pY * (pY - yA) * (pY - yB) * (pY - yC));
+
+            Console.WriteLine("Área do triângulo X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área do triângulo Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+    }
+}
