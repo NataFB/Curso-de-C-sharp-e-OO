8d55a1fce73fb205bb8b240f7118837a38a0327c
NataFB <natafbt@gmail.com>
2026-01-23 13:48:55 -0300

    Começando a aprofundar em classes

---
+++ Construtores-sobrecarga-this-encapsulamento/Construtores/Construtores/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+namespace Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Produto p = new Produto(nome, preco); // Usando o construtor com dois argumentos
+
+            Produto p2 = new Produto(); // Usando o construtor padrão
+
+
+            Produto p3 = new Produto("TV", 500.00, 20); // Usando o construtor com três argumentos
+
+            Produto p4 = new Produto  // Usando sintaxe alternativa para inicizalização de objeto. Funciona sem contrutores na classe ou com o Construtor padrão declarado como uma das posibilidades caso haja algum personalizado.
+            {
+                Nome = nome,
+                Preco = preco,
+                Quantidade = 10
+            };
+
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
---
+++ Construtores-sobrecarga-this-encapsulamento/UtilizandoThis/UtilizandoThis/Produto.cs
@@ -0,0 +1,51 @@
+﻿// this serve para referenciar o proprio objeto da classe. Usos comuns:
+// 1. Diferenciar atributos e parametros quando eles possuem o mesmo nome
+// 2. Chamar outro construtor da mesma classe
+//Passar o proprio objeto como argumento na chamada de um método ou construtor
+
+using System.Globalization;
+namespace UtilizandoThis 
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto()
+        {
+            Quantidade = 0;
+        }
+        public Produto(string Nome, double preco) : this() // chama o construtor ja existente, nesse caso o construtor padrão
+        {
+            this.Nome = Nome; // o this diferencia o atributo do parametro
+            Preco = preco;
+        }
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco) // chama o construtor ja existente com dois parametros
+        {
+            Quantidade = quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
