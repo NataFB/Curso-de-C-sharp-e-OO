6c33cfec6da7a6e0e891a93fb8b9d02df0b1ed92
NataFB <natafbt@gmail.com>
2026-01-27 11:36:00 -0300

    Exercicio de fixação de construtores,encapsulamento, this, sobrecarda ...

---
+++ Construtores-sobrecarga-this-encapsulamento/ExercicioFixacao/ExercicioFixacao/ContaBancaria.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacao
+{
+    class ContaBancaria
+    {
+        private string _nomeTitular;
+        public int NumeroConta { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numeroConta, string nomeTitular)
+        {
+            NumeroConta = numeroConta;
+            _nomeTitular = nomeTitular;
+            Saldo = 0.0;
+        }
+
+        public ContaBancaria(int numeroConta, string nomeTitular, double depositoInicial) : this(numeroConta, nomeTitular)
+        {
+            Depositar(depositoInicial);
+        }
+
+        public string NomeTitular
+        {
+            get { return _nomeTitular; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nomeTitular = value;
+                }
+            }
+        }
+
+        public void Depositar(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Sacar(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroConta}, Titular: {_nomeTitular}, Saldo: $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
---
+++ Construtores-sobrecarga-this-encapsulamento/ExercicioFixacao/ExercicioFixacao/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+
+            Console.Write("Informe o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Informe o nome do titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resposta = char.Parse(Console.ReadLine().ToLower());
+
+            if (resposta == 's')
+            {
+                Console.Write("Informe o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numeroConta, nomeTitular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numeroConta, nomeTitular);
+            }
+
+            
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: " + conta);
+
+            Console.WriteLine();
+            Console.Write("Informe um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Depositar(quantia);
+            Console.WriteLine("Dados da conta atualizados: " + conta);
+
+            Console.WriteLine();
+            Console.Write("Informe um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Sacar(quantia);
+            Console.WriteLine("Dados da conta atualizados: " + conta);
+        }
+    }
+}
