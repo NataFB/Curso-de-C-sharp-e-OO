6265dfdf1f0f63b746eba211d30b97cfa128b722
NataFB <natafbt@gmail.com>
2026-01-30 12:06:00 -0300

    Trabalhando com matrizes

---
+++ Memoria, array, listas/ExercicioFixacaoMatrizes/ExercicioFixacaoMatrizes/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ExercicioFixacaoMatrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int M = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+            int[,] mat = new int[M, N];
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] vect = Console.ReadLine().Split(" ");
+                for (int j = 0; j < N; j++)
+                {
+                    mat[i, j] = int.Parse(vect[j]);
+                }
+            }
+
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 0; i < M; i++) 
+            {
+                for (int j = 0;j < N; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Esquerda: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Cima: " + mat[i - 1, j]);
+                        }
+                        if (j < N - 1)
+                        {
+                            Console.WriteLine("Direita: " + mat[i, j + 1]);
+                        }
+                        if (i < M - 1)
+                        {
+                            Console.WriteLine("Baixo: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Memoria, array, listas/Matriz/Matriz/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //gerando a matriz, primeiro o tipo da matriz depois declarar que é uma matriz (bidimensional) deve utilizar [,], onde [Linhas, Colunas]
+            double[,] mat = new double[2, 3];
+
+
+            Console.WriteLine(mat.Length);//Mostra quantos elementos tem no total
+
+            Console.WriteLine(mat.Rank);//Mostra o tamanho da primeira dimensão (linhas)
+
+            Console.WriteLine(mat.GetLength(0));//Mostra o tamanho da primeira dimensão (linhas)
+            Console.WriteLine(mat.GetLength(1));//Mostra a segunda dimensão (colunas) 
+        }
+    }
+}
