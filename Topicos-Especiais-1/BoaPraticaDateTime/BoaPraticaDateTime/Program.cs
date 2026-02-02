b202061cdd56de3e7ababcd9b7fe5becddb35b8e
NataFB <natafbt@gmail.com>
2026-02-02 13:23:22 -0300

    Boas praticas com datetime e iso8601

---
+++ Topicos-Especiais-1/BoaPraticaDateTime/BoaPraticaDateTime/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace BoaPraticaDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local); // Instanciando manualment no horario local com Kind.Local
+
+            DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc); // Instanciando manualmente no horario UTC com Kind.Utc
+
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58); // Sem especificar
+
+            Console.WriteLine("d1: " + d1);
+            Console.WriteLine("d1 Kind: " + d1.Kind);
+            Console.WriteLine("d1 to Local: " + d1.ToLocalTime()); //Muda para o horário local da máquina (nesse caso não muda nada por ja estar no local)
+            Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime()); // Transforma para o Horário em UTC equivalentee.
+            Console.WriteLine();
+
+            Console.WriteLine("d2: " + d2);
+            Console.WriteLine("d2 Kind: " + d2.Kind);
+            Console.WriteLine("d2 to Local: " + d2.ToLocalTime()); // Nesse caso irá mudar transformando para o horário local da máquina
+            Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime()); // Nesse caso não irá mudar pois ja foi iniciado em UTC
+            Console.WriteLine();
+
+            Console.WriteLine("d3: " + d3);
+            Console.WriteLine("d3 Kind: " + d3.Kind);
+            Console.WriteLine("d3 to Local: " + d3.ToLocalTime()); // Como ele não sabe quanto é ele vai ou subtrair ou somar de acordo com o Utc local
+            Console.WriteLine("d3 to Utc: " + d3.ToUniversalTime()); // Como ele não sabe quanto é ele vai ou subtrair ou somar de acordo com o Utc Marco 0
+        }
+    }
+}
---
+++ Topicos-Especiais-1/FormatandoISO-8601/FormatandoISO-8601/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Iso8601
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            // yyyy-MM-ddTHH:mm:ssZ
+            // Z indica que a data/ hora está em Utc
+
+                        DateTime d1 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d2 = DateTime.Parse("2000-08-15T13:05:58Z"); // cria local DateTime
+
+            Console.WriteLine("d1: " + d1);
+            Console.WriteLine("d1 Kind: " + d1.Kind);
+            Console.WriteLine("d1 to Local: " + d1.ToLocalTime()); //muda por ser indefinido, gerando algo errado
+            Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime()); //muda por ser indefinido, gerando algo errado
+            Console.WriteLine();
+
+            Console.WriteLine("d2: " + d2);
+            Console.WriteLine("d2 Kind: " + d2.Kind);
+            Console.WriteLine("d2 to Local: " + d2.ToLocalTime()); // não muda por ser local
+            Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime()); // muda para UTC
+            Console.WriteLine();
+
+            Console.WriteLine(d2.ToString("yyyy-MM-ddTHH:mm:ssZ")); // cuidado! método errado, ele irá dar ja com a subtração ou adição
+            Console.WriteLine(d2.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")); // Método certo! Precisa garantir a conversão para global para assim usar a formatação
+        }
+    }
+}
