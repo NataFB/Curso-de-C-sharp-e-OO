5d56467dff3b6d619bc8c0d09d69ced1a8f1d5df
NataFB <natafbt@gmail.com>
2026-02-02 11:46:45 -0300

    Funções para string e DateTime

---
+++ Topicos-Especiais-1/DateTime/DateTime/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks);
+
+            DateTime d2 = new DateTime(2000, 8, 15); // ano mes dia
+            DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58); // ano mes dia hora minutos segundos
+            DateTime d4 = new DateTime(2000, 8, 15, 13, 5, 58, 275); // o de cima + milisegundo
+
+            DateTime d5 = DateTime.Today;
+            DateTime d6 = DateTime.UtcNow;
+
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4); // por padrão não imprime os milisegundos
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+
+            DateTime d7 = DateTime.Parse("2000-08-15"); // Converte texto para data aceita varios formatos
+            Console.WriteLine(d7);
+            DateTime d8 = DateTime.Parse("200-08-15 13:05:58");
+            Console.WriteLine(d8);
+            DateTime d9 = DateTime.Parse("11-01-2000"); //Formato BR
+            Console.WriteLine(d9);
+
+            //Determinando seu próprio formato
+            DateTime d10 = DateTime.ParseExact("2000-08-15", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            Console.WriteLine(d10);
+
+            DateTime d11 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+            Console.WriteLine(d11);
+        }
+    }
+}
---
+++ Topicos-Especiais-1/FuncoesParaString/FuncoesParaString/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace FuncoesParaString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "abcde FGHIJ ABC abc DEFG   ";
+
+
+            string s1 = original.ToUpper(); // Tudo Maiúsculo
+            string s2 = original.ToLower(); // Tudo Minúsculo
+            string s3 = original.Trim(); // Apaga espaços em branco antes e depois da frase
+            int n1 = original.IndexOf("bc"); // Busca primeira ocorrência, retorna posição da ocorrência
+            int n2 = original.LastIndexOf("bc"); // Busca Ultima ocorrência, retorna posição da ocorrência
+            string s4 = original.Substring(3); // Dando indice inicial. Gera Uma string da posição em diante, cortando antes do indice
+            string s5 = original.Substring(3, 5); // Da o inicio e quantidade de caracteres que irá cortar. Corta e gera uma nova string com os caracteres cortados.
+            string s6 = original.Replace('a', 'x'); // Troca todas as ocorrências por outra. (troca a por x)
+            string s7 = original.Replace("abc", "xy");// Troca todas as ocorrências por outra. (troca abc por xy)
+            bool b1 = String.IsNullOrEmpty(original); // Confere se é vazia ou não
+            bool b2 = String.IsNullOrWhiteSpace(original); // Confere se é vazia ou é só espaço em branco.
+
+
+            Console.WriteLine("Original: -" + original + "-");
+            Console.WriteLine("ToUpper: -" + s1 + "-");
+            Console.WriteLine("ToLower: -" + s2 + "-");
+            Console.WriteLine("Trim: -" + s3 + "-");
+            Console.WriteLine("IndexOf('bc'): " + n1);
+            Console.WriteLine("LastIndexOf('bc'): " + n2);
+            Console.WriteLine("Substring(3): -" + s4 + "-");
+            Console.WriteLine("Substring(3, 5): -" + s5 + "-");
+            Console.WriteLine("Replace('a', 'x'): -" + s6 + "-");
+            Console.WriteLine("Replace('abc', 'xy'): -" + s7 + "-");
+            Console.WriteLine("IsNullOrEmpty: " + b1);
+            Console.WriteLine("IsNullOrWhiteSpace: " + b2);
+        }
+    }
+}
