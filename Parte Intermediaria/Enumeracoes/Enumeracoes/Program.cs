f75f589a0e4ca8604bc2eb49910023b35111d1c3
NataFB <natafbt@gmail.com>
2026-02-03 10:32:07 -0300

    Enumeracoes

---
+++ Parte Intermediaria/Enumeracoes/Enumeracoes/Program.cs
@@ -0,0 +1,27 @@
+﻿using Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
+
+namespace Enumeracoes
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString(); // Convertendo valor tipo enum para string
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");// Recebe uma string e converte para enum
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+    }
+}
