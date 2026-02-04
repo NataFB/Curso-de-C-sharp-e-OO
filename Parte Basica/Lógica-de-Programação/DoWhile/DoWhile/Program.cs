a6919659626239e34e4d862e530dcd06011bf5e0
NataFB <natafbt@gmail.com>
2026-02-04 10:54:18 -0300

    Adicionando Do While

---
+++ Parte Basica/Lógica-de-Programação/DoWhile/DoWhile/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // O Do While executa o bloco de código pelo menos uma vez antes de verificar a condição. Então mesmo que a condição seja falsa desde o início, o código dentro do loop será executado uma vez.
+
+            int contador = 0;
+            do
+            {
+                Console.WriteLine("Do While!");
+            }while (contador > 5); // A condição é falsa desde o início, mas o bloco de código será executado uma vez.
+
+            Console.WriteLine("Finalizando");
+        }
+    }
+}
