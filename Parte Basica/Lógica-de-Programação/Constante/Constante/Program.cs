611a6b00442a222132817d1264ab54d1553c5b3a
NataFB <natafbt@gmail.com>
2026-02-04 10:14:44 -0300

    Constantes

---
+++ Parte Basica/Lógica-de-Programação/Constante/Constante/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Constante
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Constantes são valores fixos que não podem ser alterados durante a execução do programa.
+            // Elas são declaradas usando a palavra-chave const antes do tipo de dado.
+
+            const double Pi = 3.14;
+            //Pi = 2.00; // Isso causará um erro de compilação, pois Pi é uma constante e seu valor não pode ser alterado.
+
+            Console.WriteLine(Pi);
+        }
+    }
+}
