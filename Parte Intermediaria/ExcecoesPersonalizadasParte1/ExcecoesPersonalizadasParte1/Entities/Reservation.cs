9925c1df8f66c9143b862b727f9fac9d21029c22
NataFB <natafbt@gmail.com>
2026-02-11 14:16:56 -0300

    versão ruim de tratamento de exceções

---
+++ Parte Intermediaria/ExcecoesPersonalizadasParte1/ExcecoesPersonalizadasParte1/Entities/Reservation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExcecoesPersonalizadasParte1.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            //Calculando a duração da reserva subtraindo a data de check-in da data de check-out, o que resulta em um objeto TimeSpan que representa o intervalo de tempo entre as duas datas.
+            TimeSpan duration = CheckOut.Subtract(CheckIn); 
+            return (int)duration.TotalDays; //Convertendo o resultado para inteiro, pois a duração deve ser expressa em dias inteiros e não em double.
+        }
+
+        public void updateDates(DateTime checkIn, DateTime checkOut)
+        {
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+} 
---
+++ Parte Intermediaria/ExcecoesPersonalizadasParte1/ExcecoesPersonalizadasParte1/Program.cs
@@ -0,0 +1,48 @@
+﻿using ExcecoesPersonalizadasParte1.Entities;
+
+namespace ExcecoesPersonalizadasParte1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicitando ao usuário que insira os detalhes da reserva, como número do quarto, data de check-in e data de check-out.
+            Console.Write("Room number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in date.");
+            }else
+            {
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+                Console.WriteLine();
+                Console.Write("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if (checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("Error in reservation: Reservation dates for update must be future dates.");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Error in reservation: Check-out date must be after check-in date.");
+                }
+                else
+                {
+                    reservation.updateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation);
+                }
+            }
+        }
+    }
+}
