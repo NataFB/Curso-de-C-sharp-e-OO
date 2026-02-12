619370bce21b4381bef59e323667609ed768e86a
NataFB <natafbt@gmail.com>
2026-02-12 13:32:56 -0300

    Começando a trabalhar com arquivos

---
+++ Parte Intermediaria/Trabalhando com arquivos/FilestrameStreamreader/FilestrameStreamreader/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+
+namespace FilestrameStreamreader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Caminho do arquivo a ser lido
+            string path = @"C:\Users\nataf\OneDrive\Documentos\Curso-de-C-sharp-e-OO\Parte Intermediaria\Trabalhando com arquivos\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try 
+            {
+                // Criando o FileStream para ler o arquivo.
+                // (caminho do arquivo, modo de abertura)
+                fs = new FileStream(path, FileMode.Open);
+                // Criando o StreamReader para ler o conteúdo do arquivo.
+                sr = new StreamReader(fs);
+                while (!sr.EndOfStream)
+                {
+                    // Lendo o conteúdo do arquivo e exibindo no console.
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("An error accurred");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Fechando o StreamReader e o FileStream para liberar os recursos.
+                // Como eles não são gerenciados pelo clr do .NET pois são recursos externos, nesse caso conversa diretamente com o sistema operacional.
+                // É necessário fechá - los para liberar os recursos alocados por eles.
+                // Verificando se o StreamReader e o FileStream foram criados antes de tentar fechá-los.
+                if (sr != null) sr.Close();
+                if(fs != null) fs.Close();
+            }
+
+        }
+    }
+}
---
+++ Parte Intermediaria/Trabalhando com arquivos/UsandoFileeFileInfo/UsandoFileeFileInfo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Usandofilefileinfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Coloque o caminho do arquivo que deseja ler. O @ é para evitar erros de escape de caracteres, como a barra invertida.
+            string sourcePath = @"C:\Users\nataf\\OneDrive\Documentos\Curso-de-C-sharp-e-OO\Parte Intermediaria\Trabalhando com arquivos\file1.txt";
+
+            // Coloque o caminho do arquivo de destino.
+            string targetPath = @"C:\Users\nataf\\OneDrive\Documentos\Curso-de-C-sharp-e-OO\Parte Intermediaria\Trabalhando com arquivos\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath); // Cria um objeto FileInfo para o arquivo de origem.
+                fileInfo.CopyTo(targetPath); // Copia o arquivo para o caminho de destino.
+                string[] lines = File.ReadAllLines(sourcePath); // Lê todas as linhas do arquivo de origem e armazena em um array de strings.
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line); // Imprime cada linha do arquivo de origem no console.
+                }
+            }
+            catch (IOException e) // IOException é a classe base para erros de entrada/saída, como problemas de leitura ou escrita em arquivos.
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
