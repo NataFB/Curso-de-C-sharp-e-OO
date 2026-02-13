ff0189b2a2cc0ed9a0b108d6ba81c4f158bd64ab
NataFB <natafbt@gmail.com>
2026-02-13 11:55:19 -0300

    Utilizando Path mais exercicio de fixacao

---
+++ Parte Intermediaria/Trabalhando com arquivos/ExercicioFixacaoArquivos/ExercicioFixacaoArquivos/Program.cs
@@ -0,0 +1,55 @@
+﻿using ExercicioFixacaoArquivos.Entities;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace ExercicioFixacaoArquivos 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicita ao usuário o caminho completo do arquivo de origem
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                // Lê todas as linhas do arquivo informado
+                // Cada linha do arquivo vira uma posição do array "lines"
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);  // Obtém o caminho da pasta onde o arquivo de origem está localizado
+                string targetFolderPath = sourceFolderPath + @"\out"; // Define o caminho da pasta de saída chamada "out"
+                string targetFilePath = targetFolderPath + @"\summary.csv";  // Define o caminho completo do arquivo de saída "summary.csv" dentro da pasta "out"
+
+                // Cria a pasta "out" caso ela ainda não exista
+                // Se já existir, nada acontece (não gera erro)
+                Directory.CreateDirectory(targetFolderPath);
+
+                // Abre o arquivo summary.csv em modo de acréscimo (append)
+                // Se o arquivo não existir, ele será criado
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+
+                        string[] fields = line.Split(','); // Divide a linha em partes usando a vírgula como separador.Ex:"TV,900.00,1" → ["TV", "900.00", "1"]
+                        string name = fields[0]; // O nome do produto é a primeira parte da linha (posição 0 do array "fields")
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture); // Segundo campo: preço do produto
+                        int quantity = int.Parse(fields[2]);  // Terceiro campo: quantidade do produto
+
+                        Product prod = new Product(name, price, quantity); // Cria um objeto do tipo Product usando os dados lidos do arquivo
+
+                        sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));// Escreve no arquivo summary.csv
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Parte Intermediaria/Trabalhando com arquivos/UtilizandoPath/UtilizandoPath/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace UtilizandoPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\nataf\OneDrive\Documentos\Curso-de-C-sharp-e-OO\Parte Intermediaria\Trabalhando com arquivos\PastaTesteDirectory\file1.txt";
+            //O método Path fornece informações sobre caminhos de arquivos e diretórios, além de métodos para manipular esses caminhos.
+            //Ele é útil para lidar com caminhos de forma independente do sistema operacional, garantindo que o código funcione corretamente em diferentes ambientes.
+
+            // Exibindo informações sobre o caminho
+            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);// Retorna o caractere usado para separar diretórios em um caminho (por exemplo, '\' no Windows e '/' no Linux).
+            Console.WriteLine("PathSeparator: " + Path.PathSeparator);//Retorna o caractere usado para separar caminhos em uma lista de caminhos (por exemplo, ';' no Windows e ':' no Linux).
+            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path)); //Obtem a parte do caminho que representa o diretório.
+            Console.WriteLine("GetFileName: " + Path.GetFileName(path));//Obtem a parte do caminho que representa o nome do arquivo, incluindo a extensão.
+            Console.WriteLine("GetExtension: " + Path.GetExtension(path));//Obtem a extensão do arquivo, incluindo o ponto (por exemplo, ".txt").
+            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));//Obtem o nome do arquivo sem a extensão.
+            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));//Obtem o caminho completo, resolvendo quaisquer partes relativas (como "..") e retornando um caminho absoluto.
+            Console.WriteLine("GetTempPath: " + Path.GetTempPath());//Obtem o caminho do diretório temporário do sistema, onde arquivos temporários podem ser criados.
+        }
+    }
+}
