bfd521c51c77c1ab930ad7e17c3ab41266cdfcb6
NataFB <natafbt@gmail.com>
2026-02-19 10:54:45 -0300

    Fazendo exemplo de aluguel de carro sem interfaces

---
+++ Parte Intermediaria/Interfaces/AluguelCarroSemInterfaces/AluguelCarroSemInterfaces/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using AluguelCarroSemInterfaces.Entitites;
+using AluguelCarroSemInterfaces.Services;
+
+namespace AluguelCarroSemInterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada de dados
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            //O método ParseExact é utilizado para converter uma string em um objeto DateTime, utilizando um formato específico.
+            //O formato "dd/MM/yyyy HH:mm" define dia, mês, ano, hora (24h) e minutos.
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //Criando um objeto CarRental, passando os dados de início, término e o objeto Vehicle com o modelo do carro.
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            //Iniciando o RentalService
+            RentalService rentalService = new RentalService(hour, day);
+
+            //Processando a fatura do aluguel, passando o objeto CarRental como argumento. O método ProcessInvoice irá calcular o valor total do aluguel e gerar a fatura correspondente (gera um objeto Invoice).
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE:");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
---
+++ Parte Intermediaria/Interfaces/AluguelCarroSemInterfaces/AluguelCarroSemInterfaces/Services/RentalService.cs
@@ -0,0 +1,41 @@
+﻿using AluguelCarroSemInterfaces.Entitites;
+
+namespace AluguelCarroSemInterfaces.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerday { get; private set; }
+
+        // Criando uma dependencia direta com a classe BrazilTaxService, ou seja, a classe RentalService tem uma instância da classe BrazilTaxService.
+        // Obs: Essa é uma abordagem considerada ruim (mas por motivos de didática do curso, vai ser feito dessa forma). 
+        //Dependência direta cria forte acoplamento com BrazilTaxService,
+        // dificultando testes, reutilização e troca da regra de imposto.
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
+        public RentalService(double pricePerHour, double pricePerday)
+        {
+            PricePerHour = pricePerHour;
+            PricePerday = pricePerday;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start); // Calcula a duração do aluguel subtraindo a data de início da data de término.
+
+            double basicPayment = 0.0;
+            if (duration.TotalHours <= 12.0) //TotalHours calcula o total em horas
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours); //Math.Ceiling arredonda para cima então 4:10h ficaria 5h
+            }
+            else
+            {
+                basicPayment = PricePerday * Math.Ceiling(duration.TotalDays); // Arredonda para cima o dia nesse caso se ultrapassar das 12h
+            }
+
+            double tax = _brazilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
