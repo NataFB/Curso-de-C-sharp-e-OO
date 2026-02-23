bfda98b5201da9d22a61aad24baa6d9685639d31
NataFB <natafbt@gmail.com>
2026-02-23 11:24:03 -0300

    usando Interface IComparable

---
+++ Parte Intermediaria/Interfaces/IComparable/IComparable/Entities/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Icomparable.Entities
+{
+    internal class Employee : IComparable
+    {
+        public String Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee) //Recebe o funcionario no formato csv
+        {
+            string[] vect = csvEmployee.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1],CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString() 
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (!(obj is Employee)) 
+            {
+                throw new ArgumentException("Comparing error: argument is not an employee");
+            }
+            Employee other = obj as Employee;
+            return Name.CompareTo(other.Name);
+            
+        }
+    }
+}
---
+++ Parte Intermediaria/Interfaces/IComparable/IComparable/Program.cs
@@ -0,0 +1,35 @@
+﻿using Icomparable.Entities;
+
+namespace Icomparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\nataf\OneDrive\Documentos\Curso-de-C-sharp-e-OO\Parte Intermediaria\Interfaces\in.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    // gera e adiciona na lista lendo o arquivo:
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort(); // Ordena a list, o objeto da lista tem que implementar a interface comparable para funcionar.
+                    //Printando:
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
