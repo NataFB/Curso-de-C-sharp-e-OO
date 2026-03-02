2865097a6023bd3fb664ccc5e7cfcf36d70fc21d
NataFB <natafbt@gmail.com>
2026-03-02 11:30:49 -0300

    Iniciando com generics

---
+++ Parte Intermediaria/Generics-set-Dictionary/Generics1/Generics1/PrintService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics1
+{
+    // Generics: permite que classes, interfaces e métodos possam ser parametrizados por tipo.
+    //por ser generic irá ser declarado o tipo específico depois assim podendo reutilizar com segurança(type safety) a classe para varios tipos.
+    class PrintService <T> //Instanciando a classe generic com o <T>, que vai ser o tipo genérico da classe
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public T First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0]; 
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if(_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+        }
+
+
+
+    }
+}
---
+++ Parte Intermediaria/Generics-set-Dictionary/Generics1/Generics1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Generics1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarando o tipo do PrintService que quero utilizar.
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) 
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            int a = printService.First();
+            int b = a + 2;
+            Console.WriteLine(b);
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
