fee4cddcca67ca7245c9d602553ae611a43bed1d
NataFB <natafbt@gmail.com>
2026-03-02 13:56:30 -0300

    GetHashCode e Equals, e renomeando algumas pastas

---
+++ Parte-Intermediaria/Generics-set-Dictionary/GetHashCodeEquals/gethashcode/Entities/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetHashCodeEquals.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
+        // Sobrescrevendo com a nova lógica de comparação para a classe utilizando Equals
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is Client client))
+            {
+                return false;
+            }
+            Client other = obj as Client;
+            return Email.Equals(other.Email);
+        }
+
+
+        // Sobrescrevendo o hashcode para ser gerado com base no email assim se tiver o mesmo email o hashcode é igual
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ Parte-Intermediaria/Generics-set-Dictionary/GetHashCodeEquals/gethashcode/Program.cs
@@ -0,0 +1,20 @@
+﻿using GetHashCodeEquals.Entities;
+
+namespace GetHashCodeEquals
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
+            
+            // O Equals e o GetHashCode comparam o conteudo com base em alguma lógica (nesse caso foi sobrescrito a lógica na classe Client).
+            //O == Compara a referencia do ponteiro de memória do objeto, então mesmo se o conteudo for igual a referencia vai ser diferente.
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
