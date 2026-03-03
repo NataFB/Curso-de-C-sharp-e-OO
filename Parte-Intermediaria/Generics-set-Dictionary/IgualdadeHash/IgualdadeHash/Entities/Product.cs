bc90ca22970e431a5c01a197710f7c158f2e2d73
NataFB <natafbt@gmail.com>
2026-03-03 14:16:44 -0300

    Igualdade Hash

---
+++ Parte-Intermediaria/Generics-set-Dictionary/IgualdadeHash/IgualdadeHash/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿namespace IgualdadeHash.Entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        //criando lógica de comparação do gethashcode
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + Price.GetHashCode();
+        }
+
+        //criando lógica de comparação do Equals
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is Product))
+            {
+                return false;
+            }
+            Product other = obj as Product;
+            return Name.Equals(other.Name) && Price.Equals(other.Price);
+        }
+    }
+}
