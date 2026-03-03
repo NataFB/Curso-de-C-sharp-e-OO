62521c48eda946c1ca1b1f1a2ad6327495971064
NataFB <natafbt@gmail.com>
2026-03-03 14:15:10 -0300

    Iniciando com Hash e SortedSet

---
+++ Parte-Intermediaria/Generics-set-Dictionary/HashSetSortedSet/HashSetSortedSet/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HashSetSortedSet
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            HashSet<string> set = new HashSet<string>(); //instanciando
+
+            //adicionando:
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            Console.WriteLine(set.Contains("Notebook")); //set.Contains verifica se tem na lista e retorna true or false
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
