3cf4218bf80f2f7335fc2c8be6a8ff81f6ff0c5c
NataFB <natafbt@gmail.com>
2026-03-31 11:49:26 -0300

    Tipos de delegate: Predicate

---
+++ Parte-Avancada/PredicateDelegate/PredicateDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using PredicateDelegate.entities;
+
+namespace PredicateDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            // O PREDICATE É UMA DELEGATE QUE RECEBE UM OBJETO DO TIPO ESPECIFICADO COMO PARAMETRO E RETORNA UM VALOR BOOLEANO
+            //Pode usar diretamente a expressão lambda, sem a necessidade de criar um método separado
+            //list.RemoveAll(p => p.Price >= 100.0);
+            // Ou pode criar um método separado para a lógica de teste e passar o nome do método como argumento para o RemoveAll
+            list.RemoveAll(ProductTest);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
+        }
+    }
+}
---
+++ Parte-Avancada/PredicateDelegate/PredicateDelegate/entities/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace PredicateDelegate.entities
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Product: {Name}, Price: {Price}";
+        }
+    }
+}
