1ca2e9c06af1ad6f64f20f11e47027a777d939c3
NataFB <natafbt@gmail.com>
2026-04-06 10:39:20 -0300

    iniciando com LINQ

---
+++ Parte-Avancada/IntroducaoLinq/IntroducaoLinq/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace IntroducaoLinq
+{
+    class Program
+    {
+        //Linq - Language Integrated Query é uma poderosa ferramenta de consulta integrada à linguagem C#. Ele permite que você escreva consultas de forma concisa e expressiva para manipular dados de diversas fontes, como arrays, listas, bancos de dados, XML, etc. O Linq oferece uma sintaxe fluente e fácil de entender, tornando o código mais legível e eficiente.
+        static void Main(string[] args)
+        {
+            //ESPECIFICANDO A FONTE DE DADOS (Data Source)
+            //array de números inteiros. Poderia ser uma coleção de objetos, um banco de dados, um arquivo XML, etc.
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //DEFININDO A EXPRESSÃO DE CONSULTA (Query Expression)
+            //foi usado var pois o tipo que result retorna é um IEnumerable<int> e não um int. Linq retorna IEnumerable<T> ou IQueryable<T>, dependendo da fonte de dados, e não um tipo específico como int ou string. O tipo exato do resultado pode variar com base na consulta e na fonte de dados, mas geralmente é uma coleção de elementos que correspondem ao tipo dos dados originais. Porém é sempre um mais genérico, como IEnumerable<T> ou IQueryable<T>, que pode ser iterado ou consultado posteriormente.
+            var result = numbers
+                .Where(x => x % 2 == 0) // O método Where é usado para filtrar da coleção de números, selecionando apenas o que estiver conforme a condição especificada.
+                .Select(x => x * 10); // O método Select é usado para projetar os elementos filtrados.
+
+            //EXECUTANDO A CONSULTA (Executing the Query)
+            foreach (int x in result) 
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+};
---
+++ Parte-Avancada/Operações do LINQ.md
@@ -0,0 +1,27 @@
+### Operações do LINQ:
+
+
+
+* **Filtering:** Where, OfType
+* **Sorting:** OrderBy, OrderByDescending, ThenBy, ThenByDescending, Reverse
+* **Set:** Distinct, Except, Intersect, Union
+* **Quantification:** All, Any, Contains
+* **Projection:** Select, SelectMany
+* **Partition:** Skip, Take
+* **Join:**  Join, GroupJoin
+* **Grouping:** GroupBy
+* **Generational:** Empty
+* **Equality:** SequenceEquals
+* **Element:** ElementAt, First, FirstOrDefault, Last, LastOrDefault, Single, SingleOrDefault
+* **Conversions:** AsEnumerable, AsQueryable
+* **Concatenation:** Concat
+* **Aggregation:** Aggregate, Average, Count, LongCount, Max, Min, Sum
+
+#### Referências
+
+• [https://code.msdn.microsoft.com/101-LINQ-Samples-3fb9811b/view/SamplePack/1?sortBy=Popularity](https://code.msdn.microsoft.com/101-LINQ-Samples-3fb9811b/view/SamplePack/1?sortBy=Popularity)
+
+• [https://code.msdn.microsoft.com/101-LINQ-Samples-3fb9811b/view/SamplePack/2?sortBy=Popularity](https://code.msdn.microsoft.com/101-LINQ-Samples-3fb9811b/view/SamplePack/2?sortBy=Popularity)
+
+• [https://odetocode.com/articles/739.aspx](https://odetocode.com/articles/739.aspx)
+
